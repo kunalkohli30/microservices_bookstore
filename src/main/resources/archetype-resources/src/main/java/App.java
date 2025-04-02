fee75a46ae1552b4293495cc7d3a36d5e730af33
Kunal Kohli <kunalkohli30@gmail.com>
2025-04-02 12:39:56 +0530

    init

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.vivobook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
